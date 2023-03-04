6ec17758a5d7a74bbef8dadb24a71c0ec62732ef
Devdeep Ghosh <devarmforce01@gmail.com>
2023-03-04 11:35:06 +0530

    date checking implemented

--- routes/middleware/helpers.js
+++ routes/middleware/helpers.js
@@ -1,15 +1,20 @@
 const checkDateTime = (request, response, next) => {
   try {
-    if(request.body.event_start < request.body.event_end) {
-      console.log("Function working");
+    const currDate = _currDateFormatted()
+    if(currDate >= request.body.event_date && (request.body.event_start < request.body.event_end)) {
       next()
     } else {
-      request.flash("error", "Please select a correct time");
+      request.flash("error", "Please select a correct date time");
       response.redirect("/dashboard");
     }
   } catch (error) {
     request.flash("error", error.message)
   }
+}
+
+const _currDateFormatted = () => {
+  const currDate = new Date()
+  return currDate.toISOString().slice(0, 10)
 }
 
 module.exports = {checkDateTime};
