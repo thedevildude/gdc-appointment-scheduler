b0f4a7e6fd72b14159905bb3c4a0c8fb0e2dce21
Devdeep Ghosh <devarmforce01@gmail.com>
2023-03-04 11:35:37 +0530

    date checking bug removed

--- routes/middleware/helpers.js
+++ routes/middleware/helpers.js
@@ -1,7 +1,7 @@
 const checkDateTime = (request, response, next) => {
   try {
     const currDate = _currDateFormatted()
-    if(currDate >= request.body.event_date && (request.body.event_start < request.body.event_end)) {
+    if(currDate <= request.body.event_date && (request.body.event_start < request.body.event_end)) {
       next()
     } else {
       request.flash("error", "Please select a correct date time");
